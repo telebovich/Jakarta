1c8c928726ab94c03ea9581bfbe8228956af5c92
Robert Lebovich <robert.lebovich@gmail.com>
2013-12-02 15:25:11 +0200

    Added mocking objects for testing purposes

--- Jakarta.Tests/Services/BlogServiceTest.cs
+++ Jakarta.Tests/Services/BlogServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NUnit.Framework;
 using Ninject;
+using Jakarta.Models;
+using Moq;
 
 namespace Jakarta.Tests
 {
@@ -14,6 +16,28 @@ namespace Jakarta.Tests
 			var kernel = new StandardKernel (new CoreModule ());
 			actual = kernel.Get<BlogService> ();
 			Assert.IsNotNull (actual);
+		}
+
+		[Test]
+		public void ShouldBeAbleToCallBlogPostServiceAndGetBlogPost()
+		{
+			var expected = new PostPreview { Id = 1, Title = "A Blog Post", 
+				CreationDate = DateTime.Today, Copy = "This is a blog post" };
+			var blogPostPreviewRepositoryMock = 
+				new Mock<IBlogPostPreviewRepository> ();
+			blogPostPreviewRepositoryMock
+				.Setup (bp => bp.GetBlogPostPreview (1))
+				.Returns (new PostPreview { Id = 1, Title = "A Blog Post", 
+				CreationDate = DateTime.Today, Copy = "This is a blog post"
+			});
+			var blogPostPreviewService = 
+				new BlogService (blogPostPreviewRepositoryMock.Object);
+			var actual = blogPostPreviewService.GetBlogPostPreview (expected.Id);
+
+			Assert.AreEqual (expected.Id, actual.Id);
+			Assert.AreEqual (expected.Title, actual.Title);
+			Assert.AreEqual (expected.CreationDate, actual.CreationDate);
+			Assert.AreEqual (expected.Copy, actual.Copy);
 		}
 	}
 }
